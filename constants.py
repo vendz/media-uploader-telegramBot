70a7edeaf50e1a89433a9d6f927841798838c46b
vandit vasa <31917677+vendz@users.noreply.github.com>
2021-04-26 10:41:20 +0530

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,1 +1,2 @@
-welcome_text = '\nThanks for trying File Uploader bot!\n\nSend me an Image and I\'ll send the direct link of that!\n\nMade with ❤️ in India 🇮🇳'
+welcome_text = '\nThanks for trying Media Uploader bot!\n\nSend me an Image/Video and I\'ll send the direct link of that!\n\nMade with ❤️ in India 🇮🇳'
+donate_text = 'https://vasavandit.pythonanywhere.com/8780643036@paytm'
